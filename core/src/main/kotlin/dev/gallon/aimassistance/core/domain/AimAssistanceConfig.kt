3854a927752607346c64753a8c2e76f08ca6114a
Lilian Gallon <lilian.gallon@4sh.fr>
2022-10-14 00:59:44 +0200

    feat(config): updated default config

--- core/src/main/kotlin/dev/gallon/aimassistance/core/domain/AimAssistanceConfig.kt
+++ core/src/main/kotlin/dev/gallon/aimassistance/core/domain/AimAssistanceConfig.kt
@@ -8,15 +8,15 @@ data class AimAssistanceConfig(
     val aimBlock: Boolean = true,
     val blockRange: Double = 7.0,
     val miningInteractionDuration: Long = 500,
-    val miningAssistanceDuration: Long = 300,
-    val miningAimForce: Double = 2.5,
+    val miningAssistanceDuration: Long = 600,
+    val miningAimForce: Double = 7.0,
 
     // entity
     val aimEntity: Boolean = true,
     val entityRange: Double = 5.0,
-    val attackInteractionSpeed: Long = 2,
-    val attackInteractionDuration: Long = 2000,
-    val attackAssistanceDuration: Long = 1700,
-    val attackAimForce: Double = 4.0,
+    val attackInteractionSpeed: Double = 1.5,
+    val attackInteractionDuration: Long = 1000,
+    val attackAssistanceDuration: Long = 1100,
+    val attackAimForce: Double = 7.0,
     val stopAttackOnReached: Boolean = false
 )

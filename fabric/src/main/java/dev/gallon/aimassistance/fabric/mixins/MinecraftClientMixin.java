18358d664db22003125b97d7ff0b72f325e2b64f
Lilian Gallon <lilian.gallon@4sh.fr>
2022-11-17 21:21:12 +0100

    fix #6: Use java for mixins instead of kotlin

---
+++ fabric/src/main/java/dev/gallon/aimassistance/fabric/mixins/MinecraftClientMixin.java
@@ -0,0 +1,24 @@
+package dev.gallon.aimassistance.fabric.mixins;
+
+import dev.gallon.aimassistance.fabric.events.RenderEvent;
+import dev.gallon.aimassistance.fabric.events.SingleEventBus;
+import dev.gallon.aimassistance.fabric.events.TickEvent;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+
+    @Inject(method = "render(Z)V", at = @At("HEAD"))
+    private void renderEvent(boolean tick, CallbackInfo ci) {
+        SingleEventBus.INSTANCE.send(new RenderEvent());
+    }
+
+    @Inject(method = "tick()V", at = @At("TAIL"))
+    private void tickEvent(CallbackInfo info) {
+        SingleEventBus.INSTANCE.send(new TickEvent());
+    }
+}
--- fabric/src/main/kotlin/dev/gallon/aimassistance/fabric/events/Event.kt
+++ fabric/src/main/kotlin/dev/gallon/aimassistance/fabric/events/Event.kt
@@ -2,13 +2,13 @@ package dev.gallon.aimassistance.fabric.events
 
 sealed interface Event
 
-object RenderEvent : Event
+class RenderEvent : Event
 
-object TickEvent : Event
+class TickEvent : Event
 
 class MouseMoveEvent(
     val x: Double,
     val y: Double
 ): Event
 
-object LeftMouseClickEvent : Event
+class LeftMouseClickEvent : Event
